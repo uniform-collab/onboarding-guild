c19ce633bcdf67883554fb13ac3ce181d622d73f
Alex Shyba <alex.shyba@uniform.dev>
2025-02-14 09:01:47 -0800

    chore: hero without helpers

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,34 +1,14 @@
-import {
-  ComponentProps,
-  UniformText,
-} from "@uniformdev/canvas-next-rsc/component";
+import { ComponentProps } from "@uniformdev/canvas-next-rsc/component";
 import { ResolveComponentResultWithType } from "@/uniform/models";
 
 export const HeroComponent = ({
   title,
   description,
-  component,
-  context,
 }: ComponentProps<HeroProps>) => {
   return (
     <>
-      {/* <h1 className="title">{title}</h1>
-      <div className="description">{description}</div> */}
-      <UniformText
-        component={component}
-        context={context}
-        parameterId="title"
-        as="h1"
-        className="title"
-        placeholder="Enter hero title"
-      />
-      <UniformText
-        component={component}
-        context={context}
-        parameterId="description"
-        className="description"
-        placeholder="Enter hero description"
-      />
+      <h1 className="title">{title}</h1>
+      <div className="description">{description}</div>
     </>
   );
 };
